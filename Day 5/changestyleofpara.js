8850ae86e87ac843090a50bfeec9f368c6c359c4
harsh chaudhari <harshkc17@gmail.com>
2023-05-14 00:59:20 +0530

    changes

---
+++ Day 5/changestyleofpara.js
@@ -0,0 +1,9 @@
+function colours(p1){
+    let ptag=document.querySelector("p");
+    ptag.style.color=p1;
+}
+
+function applyfont(p1){
+    let ptag=document.querySelector("p");
+    ptag.style.fontSize=p1;
+}
